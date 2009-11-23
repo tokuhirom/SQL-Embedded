e82676d062438afac41c5c988e3e3f09a9a8cd37
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-11-23 21:31:55 +0900

    initial commit

---
+++ lib/SQL/Keyword.pm
@@ -0,0 +1,33 @@
+package SQL::Keyword;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+SQL::Keyword -
+
+=head1 SYNOPSIS
+
+  use SQL::Keyword;
+
+=head1 DESCRIPTION
+
+SQL::Keyword is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom ASDF gmail DOT comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
